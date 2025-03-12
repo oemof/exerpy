661346f5182ac25d20141bd1e7371608471dddf0
Sergio Tomasinelli <sergio.tomasinelli@campus.tu-berlin.de>
2025-03-12 18:34:30 +0100

    Fix: removed assumed values for exergoecon analysis of examples

--- examples/cgam/cgam_ebs.py
+++ examples/cgam/cgam_ebs.py
@@ -29,27 +29,4 @@ loss = {
 }
 
 ean.analyse(E_F=fuel, E_P=product, E_L=loss)
-
-# ------------------ Exergoeconomic Analysis ------------------
-
-Exe_Eco_Costs = {
-    # Component Investment Costs (currency/h)
-    "CC_Z": 68.0,  
-    "AC_Z": 753.0,         
-    "GEN_Z": 10.0,         
-    "APH_Z": 181.0,
-    "EV_Z": 159.0,
-    "PH_Z": 100.0,         
-    "EXP_Z": 753.0,
-
-    # Connection Fixed Costs (currency/W)
-    "1_c": 0.0,  # air input
-    "10_c": 4.57,  # fuel input
-    "8_c": 0.0,  # water inlet
-}
-
-exergoeco_analysis = ExergoeconomicAnalysis(ean)
-
-# Run the exergoeconomic analysis with cost inputs
-exergoeco_analysis.run(Exe_Eco_Costs=Exe_Eco_Costs, Tamb=ean.Tamb, )
-exergoeco_analysis.exergoeconomic_results()
+ean.exergy_results()
--- examples/cgam/cgam_json.py
+++ examples/cgam/cgam_json.py
@@ -29,34 +29,4 @@ loss = {
 }
 
 ean.analyse(E_F=fuel, E_P=product, E_L=loss)
-
-# ------------------ Exergoeconomic Analysis ------------------
-
-# Instantiate the ExergoeconomicAnalysis class with the existing ExergyAnalysis instance
-exa = ExergoeconomicAnalysis(ean)
-
-# Define plausible cost inputs (invented for this example)
-# Investment costs (Z_costs) for components in currency/h
-# Fixed costs (c) for connections in currency/W
-
-Exe_Eco_Costs = {
-    # Component Investment Costs (currency/h)
-    "CC_Z": 68.0,  
-    "AC_Z": 753.0,         
-    "GEN_Z": 10.0,         
-    "APH_Z": 181.0,
-    "EV_Z": 159.0,
-    "PH_Z": 100.0,         
-    "EXP_Z": 753.0,
-
-    # Connection Fixed Costs (currency/W)
-    "1_c": 0.0,  # air input
-    "10_c": 4.57,  # fuel input
-    "8_c": 0.0,  # water inlet
-}
-
-exergoeco_analysis = ExergoeconomicAnalysis(ean)
-
-# Run the exergoeconomic analysis with cost inputs
-exergoeco_analysis.run(Exe_Eco_Costs=Exe_Eco_Costs, Tamb=ean.Tamb, )
-exergoeco_analysis.exergoeconomic_results()
+ean.exergy_results()

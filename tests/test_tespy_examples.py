6d45d27b8129c457988a1045e048351ce464c088
Francesco Witte <francesco.witte@dlr.de>
2025-03-21 09:09:33 +0100

    Add automatic tests for the tespy example models

---
+++ tests/test_tespy_examples.py
@@ -0,0 +1,12 @@
+import os
+import runpy
+import pytest
+import pathlib
+
+
+path = os.path.join(os.path.dirname(__file__), "..", "examples")
+examples = pathlib.Path(path).glob('**/*tespy.py')
+
+@pytest.mark.parametrize('script', examples)
+def test_tespy_model_execution(script):
+    runpy.run_path(script)

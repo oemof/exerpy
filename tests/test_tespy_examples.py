4e24b0ad8d9b2b131aa5ef5e4cf0b6dff1cad1ff
Sergio Tomasinelli <sergio.tomasinelli@campus.tu-berlin.de>
2026-09-16 20:33:18 +0200

    Style: apply black to the tespy example tests

--- tests/test_tespy_examples.py
+++ tests/test_tespy_examples.py
@@ -27,9 +27,7 @@ def _relative_differences(path1, path2):
     for json_path in (path1, path2):
         contents = _load_json(str(json_path))
         settings = contents.get("settings", {})
-        simulator_results += [
-            ExergyAnalysis.from_json(str(json_path), **settings)
-        ]
+        simulator_results += [ExergyAnalysis.from_json(str(json_path), **settings)]
 
     sim1, sim2 = simulator_results
 
@@ -42,25 +40,17 @@ def _relative_differences(path1, path2):
     else:
         columns.append("e_PH")
 
-    df_sim1 = pd.DataFrame.from_dict(
-        sim1._connection_data, orient="index"
-    ).sort_index()[columns].dropna(how="all")
-    df_sim2 = pd.DataFrame.from_dict(
-        sim2._connection_data, orient="index"
-    ).sort_index()[columns].dropna(how="all")
+    df_sim1 = pd.DataFrame.from_dict(sim1._connection_data, orient="index").sort_index()[columns].dropna(how="all")
+    df_sim2 = pd.DataFrame.from_dict(sim2._connection_data, orient="index").sort_index()[columns].dropna(how="all")
 
-    overlapping_index = list(
-        set(df_sim1.index.tolist()) & set(df_sim2.index.tolist())
-    )
+    overlapping_index = list(set(df_sim1.index.tolist()) & set(df_sim2.index.tolist()))
     df_sim1 = df_sim1.loc[overlapping_index].round(6)
     df_sim2 = df_sim2.loc[overlapping_index].round(6)
 
     # inf means that sim2 has 0 value, comparison does not make sense there
     # and sometimes there seem to be NaN values in the dataframes, those are
     # removed as well
-    return (
-        (df_sim1 - df_sim2) / df_sim2
-    ).abs().replace(np.inf, 0).fillna(0)
+    return ((df_sim1 - df_sim2) / df_sim2).abs().replace(np.inf, 0).fillna(0)
 
 
 @pytest.mark.parametrize("script", examples, ids=lambda script: script.stem)
@@ -68,20 +58,14 @@ def test_tespy_model_execution(script):
     runpy.run_path(script)
 
     tespy_json = script.with_suffix(".json")
-    assert tespy_json.is_file(), (
-        f"Expected the script {script.name} to export its results to "
-        f"{tespy_json.name}."
-    )
+    assert tespy_json.is_file(), f"Expected the script {script.name} to export its results to " f"{tespy_json.name}."
 
     reference_jsons = [
-        reference for suffix in ("_ebs.json", "_aspen.json")
-        if (reference := tespy_json.with_name(
-            tespy_json.name.replace("_tespy.json", suffix)
-        )).is_file()
+        reference
+        for suffix in ("_ebs.json", "_aspen.json")
+        if (reference := tespy_json.with_name(tespy_json.name.replace("_tespy.json", suffix))).is_file()
     ]
-    assert reference_jsons, (
-        f"No reference results of other simulators found next to {script.name}."
-    )
+    assert reference_jsons, f"No reference results of other simulators found next to {script.name}."
 
     for reference_json in reference_jsons:
         diff = _relative_differences(tespy_json, reference_json)
